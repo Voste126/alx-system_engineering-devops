7a980a20fac47aed6ddf3f0340dd32be8ea04073
voste126 <steveaustine126@gmail.com>
2024-08-13 19:14:39 +0300

    working with advanced APIs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 """
-Module contains function 'number_of_subscribers'
-Query Reddit API and return number of subscribers.
-"""
-#!/usr/bin/python3
-import requests
-
-def number_of_subscribers(subreddit):
-    """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
     
     Args:
@@ -15,26 +7,17 @@ def number_of_subscribers(subreddit):
     
     Returns:
     int: The number of subscribers if the subreddit is valid, 0 otherwise.
-    """
+"""
+import requests
+
+def number_of_subscribers(subreddit):
     
-    # Set a custom User-Agent to avoid being blocked by Reddit
     headers = {'User-Agent': 'My Reddit API Client by /u/Plane_Ordinary_2613'}
-    
-    # Construct the API URL
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    # Send a GET request to the API
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # If the request is successful and the subreddit is valid
+
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
-        
-        # Return the number of subscribers
         return data['data']['subscribers']
-    
-    # If the subreddit is invalid or the request fails
     else:
-        # Return 0
         return 0
