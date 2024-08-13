10baa3a145835ee6ef2005a20cb8afd5addce3cf
voste126 <steveaustine126@gmail.com>
2024-08-13 18:55:57 +0300

    working with advanced APIs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,17 +5,21 @@ return the number of subscribers for a given subreddit.
 """
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Plane_Ordinary_2613)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Query Reddit API and return number of subscribers.
+    If invalid subreddit given, return 0;
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "TestAgent1"}
+    response = requests.get(url, headers=header, allow_redirects=False)
+
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    print(response)
+    json = response.json()
+    # print(json)
+    return json.get('data').get('subscribers')
