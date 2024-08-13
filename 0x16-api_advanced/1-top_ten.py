bc426a0772a8a09a057a6f233728accce212d1ec
voste126 <steveaustine126@gmail.com>
2024-08-13 19:28:48 +0300

    working with advanced APIs

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,8 @@ def top_ten(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "python3:reddit.top.ten:v1.0.0 (by /u/Plane_Ordinary_2613)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,params=params, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
