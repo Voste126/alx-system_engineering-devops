3226a9d775ea6e3a2850f93bd7643bf11b2f8f84
voste126 <steveaustine126@gmail.com>
2024-08-13 19:34:05 +0300

    working with advanced APIs

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,9 +2,12 @@
 """
 Module to query the Reddit API and print the titles of the first 10 hot posts listed for a given subreddit.
 """
+#!/usr/bin/python3
+"""
+Module to query the Reddit API and print the titles of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
-
 
 def top_ten(subreddit):
     """
@@ -13,8 +16,7 @@ def top_ten(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "python3:reddit.top.ten:v1.0.0 (by /u/Plane_Ordinary_2613)"}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers,params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
