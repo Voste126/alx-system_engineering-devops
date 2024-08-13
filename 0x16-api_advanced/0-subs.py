86e7d5d325f89200cfac7c2f53b985801621c297
voste126 <steveaustine126@gmail.com>
2024-08-13 07:11:22 -0700

    advanced APIS

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module contains function 'number_of_subscribers'
+Query Reddit API and return number of subscribers.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Query Reddit API and return number of subscribers.
+    If invalid subreddit given, return 0;
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "Plane_Ordinary_2613"}
+    response = requests.get(url, headers=header, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+    print(response)
+    json = response.json()
+    # print(json)
+    return json.get('data').get('subscribers')
