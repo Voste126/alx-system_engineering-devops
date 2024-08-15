8169c318b4c410e1707294e61e491d89218fa676
voste126 <steveaustine126@gmail.com>
2024-08-15 09:37:00 -0700

    working with apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes a wordpress site running on apache2
+# Fixes a wordpress site tha is running in the apache2 server.
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
   path    => ['/bin', '/usr/bin', '/usr/sbin']
