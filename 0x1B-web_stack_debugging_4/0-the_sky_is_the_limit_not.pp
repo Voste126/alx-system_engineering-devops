c25732583080efde1e7d88301cc773fe045da22a
voste126 <steveaustine126@gmail.com>
2024-08-20 19:34:28 +0300

    working with puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# A puppet script for the solution to the 2000 request issue on the nginx server
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
